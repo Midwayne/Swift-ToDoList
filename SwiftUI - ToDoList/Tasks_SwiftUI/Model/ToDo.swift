f76984a29db4a28d77f8103cc006c4ce27fbbd98
Kushal Krishna <kushal5070@gmail.com>
2023-04-12 12:37:40 +0530

    Add files via upload

---
+++ SwiftUI - ToDoList/Tasks_SwiftUI/Model/ToDo.swift
@@ -0,0 +1,30 @@
+//
+//  ToDo.swift
+//  Tasks_SwiftUI
+//
+//  Created by APPLE on 12/04/23.
+//
+
+import SwiftUI
+import Foundation
+import CoreData
+
+// ToDo code generation is turned OFF in the xcdatamodeld file
+public class ToDo: NSManagedObject, Identifiable {
+    @NSManaged public var title: String?
+    @NSManaged public var due: Date
+    @NSManaged public var isNotify: Bool
+}
+
+extension ToDo {
+    
+    static func allToDoFetchRequest() -> NSFetchRequest<ToDo> {
+        let request: NSFetchRequest<ToDo> = ToDo.fetchRequest() as! NSFetchRequest<ToDo>
+        
+        // The @FetchRequest property wrapper in the ContentView requires a sort descriptor
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+          
+        return request
+    }
+}
+
